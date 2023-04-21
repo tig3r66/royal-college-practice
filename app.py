f7b402a63f989477efa2d94c38e775a1b0da5625
tig3r66 <50850907+tig3r66@users.noreply.github.com>
2023-04-21 12:46:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ import json
 
 import gtts
 from playsound import playsound
+import ffmpeg
+from pydub import AudioSegment
+from pydub.playback import play
+import io
+import numpy as np
 
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -32,7 +37,7 @@ class Exam:
 
         self.option = option.strip().lower()
         self.engine = pyttsx3.init()
-        self.memory = [{"role": "system", "content": instructions}]
+        self.memory = [{"role": "user", "content": instructions}]
         self.tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
         self.tokens = len(self.tokenizer(instructions)['input_ids'])
         self.max_tokens = 4000
@@ -46,19 +51,33 @@ class Exam:
         # from cases json
         self.images = CASES[self.option.strip().lower()][0]['images'] if len(CASES[self.option.strip().lower()][0]['images']) > 0 else None
 
+    def load_audio(self, file, sr=16000):
+        if isinstance(file, bytes):
+            inp = file
+            file = 'pipe:'
+        else:
+            inp = None
+        try:
+            out, _ = (
+                ffmpeg.input(file, threads=0)
+                .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+                .run(cmd="ffmpeg", capture_stdout=True, capture_stderr=True, input=inp)
+            )
+        except ffmpeg.Error as e:
+            raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+        return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
+
     def transcribe(self, audio):
         try:
-            with open("audio.wav", "wb") as f:
-                f.write(audio.get_wav_data())
-            return self.model.transcribe('audio.wav', language='en', fp16=False)['text']
+            return self.model.transcribe(self.load_audio(audio.get_wav_data()), language='en', fp16=False)['text']
         except:
             return None
 
     def generate_response(self, prompt, user='user', pop_latest=False):
         self.update_memory(user, prompt)
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            # model='gpt-4',
+            # model="gpt-3.5-turbo",
+            model='gpt-4',
             messages=self.memory,
             temperature=0.5,
             top_p=1,)['choices'][0]['message']['content']
@@ -78,8 +97,10 @@ class Exam:
         return response
 
     def speak(self, text):
-        gtts.gTTS(text).save("output.mp3")
-        playsound("output.mp3")
+        audio = io.BytesIO()
+        gtts.gTTS(text=text).write_to_fp(audio)
+        audio.seek(0)
+        play(AudioSegment.from_file(audio, format="mp3"))
 
     def update_memory(self, role, content):
         self.memory.append({"role": role, "content": content})
@@ -88,12 +109,6 @@ class Exam:
             popped = self.memory.pop(0)
             self.tokens -= len(self.tokenizer(popped['content'])['input_ids'])
 
-    def get_user_voice(self):
-        with sr.Microphone() as source:
-            source.pause_threshold = 1.5  # silence in seconds
-            audio = self.r.listen(source)
-            return self.transcribe(audio)
-    
     def show_image(self, response, imgs_path):
         if imgs_path:
             for img_path in imgs_path:
@@ -107,32 +122,50 @@ class Exam:
 
         if st.session_state.feedback_state is False:
             with sr.Microphone() as source:
-                source.pause_threshold = 2.5  # silence in seconds
+                source.pause_threshold = 1  # silence in seconds
                 first_q = True
                 while True:
                     if stop_button:
                         break
                     if first_q:
-                        response = self.generate_response('Provide a brief history of the case. If necessary, include the image files within parentheses but do not describe them.')
+                        response = self.generate_response('Provide a brief history of the case. Do not give all the information away. If necessary, include the image files within parentheses but do not describe them.', user='system', pop_latest=True)
                         self.show_image(response, self.images)
                         st.write(f"Examiner: {response}")
                         self.speak(response)
                         first_q = False
-                    # examiner question
-                    response = self.generate_response('If the examinee asks a question or asks for further information, please answer. Then, ask question(s) about the case or ask follow-up question(s). Do not confirm this request, just directly answer the examinee.', 'system', True)
-                    self.show_image(response, self.images)
-                    st.write(f"Examiner: {response}")
-                    self.speak(response)
-                    self.history.append(f'Examiner: {response}')
-                    update_session_history(f'Examiner: {response}')
+
+                        # first question
+                        response = self.generate_response('Based on the instructions and provided case, ask a question.', user='system', pop_latest=True)
+                        self.show_image(response, self.images)
+                        self.history.append(f'Examiner: {response}')
+                        update_session_history(f'Examiner: {response}')
+                        st.write(f"Examiner: {response}")
+                        self.speak(response)
+                        continue
 
                     # user input
                     audio = self.r.listen(source)
                     text = self.transcribe(audio)
                     if text:
-                        st.write(f'Me: {text}')
+                        print('answering')
                         self.history.append(f'Me: {text}')
                         update_session_history(f'Me: {text}')
+                        st.write(f'Me: {text}')
+
+                        # examiner question
+                        response = self.generate_response(response)
+                        self.history.append(f'Examiner: {response}')
+                        update_session_history(f'Examiner: {response}')
+                        self.show_image(response, self.images)
+                        st.write(f"Examiner: {response}")
+                        self.speak(response)
+                        if '?' not in response:
+                            response = self.generate_response('Ask the examinee about the case or ask follow-up questions. Do not confirm or acknowledge this request; directly answer the examinee.', pop_latest=True)
+                            self.history.append(f'Examiner: {response}')
+                            update_session_history(f'Examiner: {response}')
+                            self.show_image(response, self.images)
+                            st.write(f"Examiner: {response}")
+                            self.speak(response)
         else:
             for i in st.session_state.history:
                 st.write(i)
@@ -145,7 +178,7 @@ class Exam:
         feedback_button = st.button('Get feedback', key='feedback')
         if feedback_button:
             if len(st.session_state.history) != 0:
-                instructions = 'Based on the chat dialogue between me and the patient, please provide constructive feedback and criticism for the resident ("Me:"), NOT the examiner. Comment on the medical accuracy of responses. Comment on things that were done well, areas for improvement, and other remarks as necessary. Do not make anything up.'
+                instructions = 'Based on the chat dialogue between me and the patient, please provide constructive feedback and criticism for the resident ("Me:"), NOT the examiner. Comment on the medical accuracy of responses. Comment on things that were done well, areas for improvement, and other remarks as necessary. Do not make anything up. If the examinee asks a question or requests more information, fulfill their request. Regardless of your initial response, ask the examinee about the case or ask follow-up questions.'
                 temp_mem = [{'role': 'user', 'content': '\n'.join(st.session_state.history) + instructions}]
                 stream = self.generate_response_stream(temp_mem)
                 t = st.empty()
@@ -176,7 +209,7 @@ def update_session_history(prompt):
         st.session_state.history.pop(0)
 
 def create_prompt(cases, option):
-    instructions = f"Instructions: You are an evaluator for a neurosurgery oral exam. Provide contextual information to the examinee as relevant, as the examinee does not have any information of the case beforehand. Speak as if you were talking the examinee. Treat this as an exam and do not provide words of encouragement. Provide hints if the examinee does not know the answer.\n\nContext:{cases[option.strip().lower()][0]['case_info']}" + "\nPlease write the image files in parentheses if you would like to use them in your questions. If you've already used an image, no need to include it in parentheses."
+    instructions = f"Instructions: You are an evaluator for a neurosurgery oral exam. Provide contextual information to the examinee as relevant, as the examinee does not have any information of the case beforehand. Speak as if you were talking the examinee. Treat this as an exam and do not provide words of encouragement. Provide hints if the examinee does not know the answer.\n\nContext:{cases[option.strip().lower()][0]['case_info']}" + "\nPlease write the image files in parentheses if you would like to use them in your questions. You do not need to use these images in the first question unless relevant. If you've already used an image, no need to include it in parentheses."
     images = cases[option.strip().lower()][0]['images']
 
     if len(images) > 0:
